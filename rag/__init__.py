e7abdee07bf57a53bd874c7d156d8c8b113d2d02
Vignesh Ponraj <vigneshponraj97@gmail.com>
2026-04-26 07:56:03 -0600

    chore: scaffold rag package and add embedding dependencies

---
+++ rag/__init__.py
@@ -0,0 +1,10 @@
+from rag.care_kb import (
+    Index,
+    Match,
+    QueryResult,
+    build_index,
+    load_index,
+    query,
+)
+
+__all__ = ["Index", "Match", "QueryResult", "build_index", "load_index", "query"]
